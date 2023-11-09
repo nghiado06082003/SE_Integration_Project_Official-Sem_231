569d2d3ebed6195bdf2457aee5032dc6da37b22b
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2023-11-09 11:29:23 +0700

    update sql

--- simbsc.sql
+++ simbsc.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 05, 2023 lúc 07:56 AM
--- Phiên bản máy phục vụ: 10.4.27-MariaDB
--- Phiên bản PHP: 8.2.0
+-- Host: 127.0.0.1
+-- Generation Time: Nov 09, 2023 at 05:28 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,74 +18,148 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `simbsc`
+-- Database: `simbsc`
 --
-CREATE DATABASE IF NOT EXISTS `simbsc` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `simbsc`;
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `borrow`
+-- Table structure for table `borrow`
 --
 
-CREATE TABLE IF NOT EXISTS `borrow` (
+CREATE TABLE `borrow` (
   `student_id` int(11) NOT NULL,
   `document_id` int(11) NOT NULL,
   `borrow_date` date NOT NULL DEFAULT current_timestamp(),
-  `return_date` int(11) DEFAULT NULL,
-  KEY `student_id` (`student_id`),
-  KEY `document_id` (`document_id`)
+  `return_date` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `documents`
+-- Table structure for table `documents`
 --
 
-CREATE TABLE IF NOT EXISTS `documents` (
-  `document_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(4096) NOT NULL,
+CREATE TABLE `documents` (
+  `document_id` int(11) NOT NULL,
+  `doc_name` varchar(4096) NOT NULL,
   `type` varchar(255) NOT NULL,
   `author` varchar(4096) NOT NULL,
   `publisher` varchar(4096) NOT NULL,
   `publish_year` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  PRIMARY KEY (`document_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `documents`
+-- Dumping data for table `documents`
 --
 
-INSERT INTO `documents` (`document_id`, `name`, `type`, `author`, `publisher`, `publish_year`, `quantity`) VALUES
+INSERT INTO `documents` (`document_id`, `doc_name`, `type`, `author`, `publisher`, `publish_year`, `quantity`) VALUES
 (1, 'Kiến trúc máy tính', 'Sách cơ sở ngành', 'Phạm Quốc Cường', 'NXB Đại học Quốc gia TPHCM', 2019, 1),
 (3, 'Cấu trúc dữ liệu và giải thuật', 'Sách cơ sở ngành', 'Nguyễn Trung Trực', 'NXB Đại học Quốc gia TPHCM', 2019, 1);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `members`
+-- Table structure for table `members`
 --
 
-CREATE TABLE IF NOT EXISTS `members` (
+CREATE TABLE `members` (
   `student_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `student_name` varchar(255) NOT NULL,
   `email` varchar(127) NOT NULL,
   `password` varchar(127) NOT NULL,
   `state` varchar(127) NOT NULL,
   `join_date` date NOT NULL DEFAULT current_timestamp(),
-  `permission` varchar(255) NOT NULL,
-  PRIMARY KEY (`student_id`)
+  `permission` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Các ràng buộc cho các bảng đã đổ
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`student_id`, `student_name`, `email`, `password`, `state`, `join_date`, `permission`) VALUES
+(0, 'Trần Văn A', 'tranvanA@hcmut.edu.vn', 'tranvana', '', '2023-11-09', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requestborrow`
+--
+
+CREATE TABLE `requestborrow` (
+  `student_id` int(11) NOT NULL,
+  `document_id` int(11) NOT NULL,
+  `request_day` date NOT NULL,
+  `state` int(11) NOT NULL DEFAULT 0,
+  `update_date` date DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `received_day` date DEFAULT NULL,
+  `returned_day` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `requestborrow`
+--
+
+INSERT INTO `requestborrow` (`student_id`, `document_id`, `request_day`, `state`, `update_date`, `id`, `received_day`, `returned_day`) VALUES
+(1, 1, '2023-11-15', 0, '2023-11-20', 1, NULL, NULL),
+(5, 3, '0000-00-00', 0, NULL, 2, NULL, NULL),
+(5, 1, '2023-12-23', 0, NULL, 3, NULL, NULL),
+(5, 2, '2023-11-08', 0, NULL, 4, NULL, NULL),
+(5, 2, '2023-11-09', 0, NULL, 5, NULL, NULL);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Các ràng buộc cho bảng `borrow`
+-- Indexes for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `document_id` (`document_id`);
+
+--
+-- Indexes for table `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`document_id`);
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `requestborrow`
+--
+ALTER TABLE `requestborrow`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `requestborrow`
+--
+ALTER TABLE `requestborrow`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `borrow`
 --
 ALTER TABLE `borrow`
   ADD CONSTRAINT `borrow_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `members` (`student_id`),
