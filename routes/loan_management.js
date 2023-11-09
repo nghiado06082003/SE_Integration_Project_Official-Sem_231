8fda818b1855b529565641885ec09dc35e960d32
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2023-11-09 16:21:07 +0700

    final commit

--- routes/loan_management.js
+++ routes/loan_management.js
@@ -5,9 +5,13 @@ const path = require("path");
 
 // For manager
 loan_management_router.get("/manager/list", loan_management_controller.getLoanList);
+loan_management_router.get("/manager/request/approve", loan_management_controller.approvereq);
+loan_management_router.get("/manager/request/deny", loan_management_controller.denyreq);
+
 // For customer
-loan_management_router.get("/customer/history", loan_management_controller.getLoanHistory);
+loan_management_router.get("/customer/loanhistory", loan_management_controller.getLoanHistory);
 loan_management_router.get("/customer/request", loan_management_controller.request);
+loan_management_router.get("/customer/borrowhistory", loan_management_controller.getBorrowHistory);
 
 
 module.exports = loan_management_router;
