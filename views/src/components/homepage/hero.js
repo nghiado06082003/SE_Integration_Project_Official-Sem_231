061a8ff74ecf56da01f753d7e30941e576120ec4
Quan Cao <quancao2310@gmail.com>
2023-11-13 20:00:44 +0700

    fix proxy and axios for doc management

--- views/src/components/homepage/hero.js
+++ views/src/components/homepage/hero.js
@@ -1,23 +1,16 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link, NavLink, Outlet } from "react-router-dom";
-import Popper from 'popper.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import hero from '../../img/hero.png'
-import { useState } from "react";
 import "./hero.css"
 
-
-export default function Hero(){
-    return (
-        <div className="hero">
-            
-            <img className="heroImg" src={hero}></img>
-
-        </div>
-    )
-
-
-
-
+export default function Hero() {
+  return (
+    <div className="hero">
+      <img 
+        className="heroImg"
+        src={hero}
+        alt="logo"
+      />
+    </div>
+  )
 }
