83d9d3c6b90dda1f98e7bab1218cc43dc8d1ec37
Quan Cao <quancao2310@gmail.com>
2023-12-23 01:12:15 +0700

    fix sign up

--- model/DAO/authorization.js
+++ model/DAO/authorization.js
@@ -1,6 +1,5 @@
 var connect_DB = require('./connect_db');
 var jwt = require("jsonwebtoken");
-var bcrypt = require("bcrypt");
 
 async function loadCurMember(req, res, next) {
   if (!req.headers.authorization) {
@@ -30,8 +29,7 @@ async function authorizeCollaborator(req, res, next) {
     req.cur_member.email,
     req.cur_member.state,
     req.cur_member.permission
-  ],
-  function (err, result, field) {
+  ], function (err, result, field) {
     if (err) {
       return res.status(500).json({ message: "Hệ thống gặp vấn đề. Vui lòng thử lại sau" });
     }
@@ -49,107 +47,8 @@ async function authorizeCollaborator(req, res, next) {
 }
 
 async function authorizeMediaMember(req, res, next) {
-    if (req.cur_member) {
-        let sql = "SELECT * FROM members WHERE student_id = ? AND email = ? AND state = ? AND permission = ?";
-        connect_DB.query(sql, [
-            req.cur_member.student_id,
-            req.cur_member.email,
-            req.cur_member.state,
-            req.cur_member.permission
-        ], function (err, result, field) {
-            if (err) {
-                res.status(500).json({ message: "Hệ thống gặp vấn đề. Vui lòng thử lại sau" });
-            }
-            else if (result.length == 0) {
-                res.status(400).json({ message: "Người dùng không tồn tại. Vui lòng kiểm tra" });
-            }
-            else {
-                if (result[0].state != "Đang hoạt động") {
-                    res.status(403).json({ message: "Người dùng đang bị khoá!" });
-                }
-                else if (result[0].permission != "Thành viên ban truyền thông" && result[0].permission != "Thành viên ban chủ nhiệm") {
-                    res.status(403).json({ message: "Người dùng không có quyền truy cập trang hay tác vụ này!" });
-                }
-                else {
-                    next();
-                }
-            }
-        })
-    }
-    else {
-        res.status(401).json({ message: "Người dùng không xác định. Vui lòng kiểm tra" });
-    }
-}
-
-async function authorizeContentMember(req, res, next) {
-    if (req.cur_member) {
-        let sql = "SELECT * FROM members WHERE student_id = ? AND email = ? AND state = ? AND permission = ?";
-        connect_DB.query(sql, [
-            req.cur_member.student_id,
-            req.cur_member.email,
-            req.cur_member.state,
-            req.cur_member.permission
-        ], function (err, result, field) {
-            if (err) {
-                res.status(500).json({ message: "Hệ thống gặp vấn đề. Vui lòng thử lại sau" });
-            }
-            else if (result.length == 0) {
-                res.status(400).json({ message: "Người dùng không tồn tại. Vui lòng kiểm tra" });
-            }
-            else {
-                if (result[0].state != "Đang hoạt động") {
-                    res.status(403).json({ message: "Người dùng đang bị khoá!" });
-                }
-                else if (result[0].permission != "Thành viên ban nội dung" && result[0].permission != "Thành viên ban chủ nhiệm") {
-                    res.status(403).json({ message: "Người dùng không có quyền truy cập trang hay tác vụ này!" });
-                }
-                else {
-                    next();
-                }
-            }
-        })
-    }
-    else {
-        res.status(401).json({ message: "Người dùng không xác định. Vui lòng kiểm tra" });
-    }
-}
-
-async function authorizeLogisticMember(req, res, next) {
-    if (req.cur_member) {
-        let sql = "SELECT * FROM members WHERE student_id = ? AND email = ? AND state = ? AND permission = ?";
-        connect_DB.query(sql, [
-            req.cur_member.student_id,
-            req.cur_member.email,
-            req.cur_member.state,
-            req.cur_member.permission
-        ], function (err, result, field) {
-            if (err) {
-                res.status(500).json({ message: "Hệ thống gặp vấn đề. Vui lòng thử lại sau" });
-            }
-            else if (result.length == 0) {
-                res.status(400).json({ message: "Người dùng không tồn tại. Vui lòng kiểm tra" });
-            }
-            else {
-                if (result[0].state != "Đang hoạt động") {
-                    res.status(403).json({ message: "Người dùng đang bị khoá!" });
-                }
-                else if (result[0].permission != "Thành viên ban hậu cần" && result[0].permission != "Thành viên ban chủ nhiệm") {
-                    res.status(403).json({ message: "Người dùng không có quyền truy cập trang hay tác vụ này!" });
-                }
-                else {
-                    next();
-                }
-            }
-        })
-    }
-    else {
-        res.status(401).json({ message: "Người dùng không xác định. Vui lòng kiểm tra" });
-    }
-}
-
-async function authorizeAdmin(req, res, next) {
   if (!req.cur_member) {
-    return res.status(401).json({ message: "Người dùng không xác định. Vui lòng kiểm tra" });
+    return res.status(400).json({ message: "Người dùng không xác định. Vui lòng kiểm tra" });
   }
   
   let sql = "SELECT * FROM members WHERE student_id = ? AND email = ? AND state = ? AND permission = ?";
@@ -158,8 +57,103 @@ async function authorizeAdmin(req, res, next) {
     req.cur_member.email,
     req.cur_member.state,
     req.cur_member.permission
-  ], 
-  function (err, result, field) {
+  ], function (err, result, field) {
+    if (err) {
+      return res.status(500).json({ message: "Hệ thống gặp vấn đề. Vui lòng thử lại sau" });
+    }
+    
+    if (result.length == 0) {
+      return res.status(400).json({ message: "Người dùng không tồn tại. Vui lòng kiểm tra" });
+    }
+    
+    if (result[0].state != "Đang hoạt động") {
+      return res.status(403).json({ message: "Người dùng đang bị khoá!" });
+    }
+    
+    if (result[0].permission != "Thành viên ban truyền thông" && result[0].permission != "Thành viên ban chủ nhiệm") {
+      return res.status(403).json({ message: "Người dùng không có quyền truy cập trang hay tác vụ này!" });
+    }
+    
+    next();
+  });
+}
+
+async function authorizeContentMember(req, res, next) {
+  if (!req.cur_member) {
+    return res.status(400).json({ message: "Người dùng không xác định. Vui lòng kiểm tra" });
+  }
+  
+  let sql = "SELECT * FROM members WHERE student_id = ? AND email = ? AND state = ? AND permission = ?";
+  connect_DB.query(sql, [
+    req.cur_member.student_id,
+    req.cur_member.email,
+    req.cur_member.state,
+    req.cur_member.permission
+  ], function (err, result, field) {
+    if (err) {
+      return res.status(500).json({ message: "Hệ thống gặp vấn đề. Vui lòng thử lại sau" });
+    }
+    
+    if (result.length == 0) {
+      return res.status(400).json({ message: "Người dùng không tồn tại. Vui lòng kiểm tra" });
+    }
+    
+    if (result[0].state != "Đang hoạt động") {
+      return res.status(403).json({ message: "Người dùng đang bị khoá!" });
+    }
+    
+    if (result[0].permission != "Thành viên ban nội dung" && result[0].permission != "Thành viên ban chủ nhiệm") {
+      return res.status(403).json({ message: "Người dùng không có quyền truy cập trang hay tác vụ này!" });
+    }
+    
+    next();
+  });
+}
+
+async function authorizeLogisticMember(req, res, next) {
+  if (!req.cur_member) {
+    return res.status(400).json({ message: "Người dùng không xác định. Vui lòng kiểm tra" });
+  }
+  
+  let sql = "SELECT * FROM members WHERE student_id = ? AND email = ? AND state = ? AND permission = ?";
+  connect_DB.query(sql, [
+    req.cur_member.student_id,
+    req.cur_member.email,
+    req.cur_member.state,
+    req.cur_member.permission
+  ], function (err, result, field) {
+    if (err) {
+      return res.status(500).json({ message: "Hệ thống gặp vấn đề. Vui lòng thử lại sau" });
+    }
+    
+    if (result.length == 0) {
+      return res.status(400).json({ message: "Người dùng không tồn tại. Vui lòng kiểm tra" });
+    }
+    
+    if (result[0].state != "Đang hoạt động") {
+      return res.status(403).json({ message: "Người dùng đang bị khoá!" });
+    }
+    
+    if (result[0].permission != "Thành viên ban hậu cần" && result[0].permission != "Thành viên ban chủ nhiệm") {
+      return res.status(403).json({ message: "Người dùng không có quyền truy cập trang hay tác vụ này!" });
+    }
+    
+    next();
+  });
+}
+
+async function authorizeAdmin(req, res, next) {
+  if (!req.cur_member) {
+    return res.status(400).json({ message: "Người dùng không xác định. Vui lòng kiểm tra" });
+  }
+  
+  let sql = "SELECT * FROM members WHERE student_id = ? AND email = ? AND state = ? AND permission = ?";
+  connect_DB.query(sql, [
+    req.cur_member.student_id,
+    req.cur_member.email,
+    req.cur_member.state,
+    req.cur_member.permission
+  ], function (err, result, field) {
     if (err) {
       return res.status(500).json({ message: "Hệ thống gặp vấn đề. Vui lòng thử lại sau" });
     }
